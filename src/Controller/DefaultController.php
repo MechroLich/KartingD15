e97f1e68cace264acb17780225b8f94bd64e7997
MechroLich <timmydunne@gmail.com>
2020-04-05 11:38:41 +0100

    pages and routes added, user class created, login and security controller created, broken logout page/function

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -16,4 +16,41 @@ class DefaultController extends AbstractController
         $args = [];
         return $this->render($template, $args);
     }
+    /**
+     * @Route("/welcome", name="welcome")
+     */
+    public function welcome()
+    {
+        $template = 'default/welcome.html.twig';
+        $args = [];
+        return $this->render($template, $args);
+    }
+    /**
+     * @Route("/laptimes", name="laptimes")
+     */
+    public function laptimes()
+    {
+        $template = 'default/laptimes.html.twig';
+        $args = [];
+        return $this->render($template, $args);
+    }
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function admin()
+    {
+        $template = 'default/admin.html.twig';
+        $args = [];
+        return $this->render($template, $args);
+    }
+    /**
+     * @Route("/staff", name="staff")
+     */
+    public function staff()
+    {
+        $template = 'default/staff.html.twig';
+        $args = [];
+        return $this->render($template, $args);
+    }
+
 }
